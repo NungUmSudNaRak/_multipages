b9a28817fd9df972c7d6a6fd1da04ca2100300c1
NungUmSudNaRak <puripat.won@spumail.net>
2024-10-23 01:14:12 +0700

    Initial commit

---
+++ src/Layout/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header-container'>
+            <span className='badge bg-dark'><h1>Csi205 front end software devolopment</h1></span>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,35 @@
+import './Home.css'
+function Home() {
+    return (
+        <div className='home-container'>
+            <h1>Introduce yourself</h1>
+            <div className='introduce-container'>
+                <div className='img-container'>
+                </div>
+                <div className='text-container'>
+                    <h2><u>NungUmSudNaRak</u></h2>
+                    <br />
+                    <p className='text'>
+                        My fullname is Puripat Wongtangton.
+                        <br />
+                        I'm 21 years old.
+                        <br />
+                        I'm student at Sripatum University.
+                        <br />
+                        I'm a programmer.
+                        <br />
+                        I'm not interested in programming.
+                        <br />
+                        My hobby is sleeping.
+                        <br />
+                        My favorite food is noodles.
+                        <br />
+                        My favorite color is black. 
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
